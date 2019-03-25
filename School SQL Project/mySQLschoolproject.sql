d9daf8b9706d6676a138bab27ba2da98e6fc87fd
Christopher <christopherstam@yahoo.com>
2019-03-25 05:32:27 -0700

    Added SQL work

---
+++ School SQL Project/mySQLschoolproject.sql
@@ -0,0 +1,20 @@
+
+
+PRINT 'Hello World!'
+PRINT 'Having fun with' + ' TSQL and MS SQL SERVER'
+
+
+
+DECLARE @myVariable INT 
+SET @myVariable = 6 
+PRINT @myVariable
+
+
+DECLARE @var1 INT, @var2 INT
+SET @var1 = 3
+SET @var2 = 5
+
+Print char(9) + 'I have ' + CONVERT(varchar(50),@var1) + CHAR(13) + ' dollars...'
+
+
+/*This is a silent message for my purpose only or anyone I want to share*/
